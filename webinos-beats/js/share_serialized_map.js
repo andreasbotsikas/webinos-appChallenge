e193b1784e6c8a1c274b8b26b270a9f131d4d044
Andreas Botsikas <abot@epu.ntua.gr>
2014-01-25 03:37:16 +0200

    User can select events service

--- webinos-beats/js/share_serialized_map.js
+++ webinos-beats/js/share_serialized_map.js
@@ -4,6 +4,11 @@ var binded = false;
 
 shareBeats.init = function() {
     webinosPlayers();
+	$('#cmdSelectEvents').bind('click', openDashboard);
+	// Initialize current selection
+	if (typeof localStorage != "undefined" && localStorage.getItem("eventsApi_zoneId") != null) {
+		$("#selectedServer").text("Current selection: " + localStorage.getItem("eventsApi_zoneId"));
+	}
     getPlayers();
     console.log("Share initialized!");
     findEvents();    
@@ -45,14 +50,43 @@ $(document).ready(function() {
 });
 
 
+function openDashboard() {
+     once = false; // Reset the once to be able to rebind to new service (although events will be coming from the previous source too
+     $("#selectedServer").text("");
+     if (typeof localStorage != "undefined") {
+         localStorage.removeItem("eventsApi_zoneId");
+         webinos.dashboard.open({
+             module: 'explorer',
+             data: {
+                 service: ['http://webinos.org/api/events'],
+                 select: "devices"
+             }
+         }).onAction(function (data) {
+             if (typeof localStorage != "undefined" && data.result.length == 1) {
+                 localStorage.setItem("eventsApi_zoneId", data.result[0].id);
+                 $("#selectedServer").text("Current server: " + data.result[0].id + ")");
+				 findEvents();
+             }
+         });
+     } else {
+         $("#selectedServer").text("Your renderer doesn't support localstorage!");
+     }
+}
+
 function findEvents(){  
+	var zoneId;
+    if (typeof localStorage != "undefined" && localStorage.getItem("eventsApi_zoneId") != null) {
+        zoneId = {
+            zoneId: [localStorage.getItem("eventsApi_zoneId")]
+        };
+    }
     webinos.ServiceDiscovery.findServices(new ServiceType('http://webinos.org/api/events'), {
         onFound: function (service) {
             //serviceArray.push(service);
             servicesArray[servicesArray.length] = service;
             bindEventService(service);
         }        
-    });
+    }, null, zoneId);
 }
 
 
